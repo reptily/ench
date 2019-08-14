8e7ce9e42ba179f732e8e8c8129ac7866b9f4031
Reptily <reptily@go2ride.ru>
2019-08-14 15:51:59 +0300

    go go go

---
+++ test.js
@@ -0,0 +1,13 @@
+const Math = require("./index.js");
+
+const v = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789-_=+)(*&^%$#@!;:?";
+let str = "aaa";
+const gen = new Math();
+
+
+let i=0;
+while(i<10){
+        str = gen.next(0,str,v);
+        console.log(str);
+        i++;
+}
