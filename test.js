b81c97e36a32363caa1178b1e37c0f312fd9a7b8
reptily <47777868+reptily@users.noreply.github.com>
2019-09-18 15:47:54 +0300

    Update test.js

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const gen = new Math();
 
 let i=0;
 while(i<10){
-        str = gen.next(0,str,v);
-        console.log(str);
-        i++;
+    str = gen.next(0,str,v);
+    console.log(str);
+    i++;
 }
